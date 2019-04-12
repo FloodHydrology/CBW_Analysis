4ffe661b9cbcb0b351db01098566c0b4a29c2ac8
Nate Jones <njones@rstudio03.research.sesync.org>
2019-04-12 09:50:05 -0400

    index

--- RScripts/II_Watershed_Shapes_Processing.R
+++ RScripts/II_Watershed_Shapes_Processing.R
@@ -22,9 +22,7 @@ library(parallel)
 library(rslurm)   
 
 #Set data dir
-data_dir<-"//storage.research.sesync.org/njones-data/Research Projects/LULC_CBW/Spatial_Data/"
-scratch_dir<-"C:\\ScratchWorkspace/"
-wbt_dir<-"C:\\WBT/whitebox_tools"
+data_dir<-"//nfs/njones-data/Research Projects/LULC_CBW/Spatial_Data/"
 
 #Download required data
 dem<-raster(paste0(data_dir,"NHDPlus02/Elev_Unit_a/elev_cm"))
@@ -35,3 +33,14 @@ HUC08<-st_read(paste0(data_dir,"NHDPlus02/Subbasin.shp"))
 #######################################################################
 #Create individual raster and polygong files for each watershed--------
 #######################################################################
+#Locate files with watershed shapes
+files<-list.files(paste0(data_dir, "sheds_unnested/"))
+index_fun<-function(n){
+  temp_watershed<-raster(paste0(data_dir, "sheds_unnested/", files[n]))
+  data.frame(wuid = unique(temp_watershed), 
+             file = files[n])
+}
+watershed_index<-mclapply(X = seq(1, length(files)), FUN=index_fun, mc.cores = detectCores())
+watershed_index<-do.call(rbind, watershed_index)
+
+
